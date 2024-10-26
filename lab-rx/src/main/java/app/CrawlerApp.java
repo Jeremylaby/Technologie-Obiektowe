133171314ae29d78fbc28c590d29db3fe0490210
Jeremy <barycki@student.agh.edu.pl>
2024-10-26 23:21:54 +0200

    lab 3 po zajęciach

---
+++ lab-rx/src/main/java/app/CrawlerApp.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CrawlerApp {
+
+    public static final String SCRAPER_API_KEY = "f8a8a55046f67fa801bf22889838c40c";
+
+    private static final List<String> TOPICS = List.of("Dark Souls 3", "Sherlock", "Poirot", "Detective Monk");
+
+
+    public static void main(String[] args) throws IOException {
+        PhotoCrawler photoCrawler = new PhotoCrawler();
+        photoCrawler.resetLibrary();
+//        photoCrawler.downloadPhotoExamples();
+        photoCrawler.downloadPhotosForQuery(TOPICS.get(0));
+//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+    }
+}
